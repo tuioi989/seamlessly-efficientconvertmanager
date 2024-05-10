0915cba94dee76d623edacde62bdb93e0155fe3b
tuioi989 <roxxiemoonpun0suy@hotmail.com>
2024-05-10 16:22:43 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let p = l1,
+    q = l2,
+    curr = dummy;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    const x = p !== null ? p.val : 0;
+    const y = q !== null ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
   }
-  return currMax;
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return dummy.next;
 }
