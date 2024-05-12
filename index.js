5e6bd8bc307bfcb3af7e7124c5ede22acfd5d63c
tuioi989 <roxxiemoonpun0suy@hotmail.com>
2024-05-12 16:27:52 +0700

    test

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-      else left = mid + 1;
-    } else {
-      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-      else right = mid - 1;
-    }
-  }
-  return -1;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
