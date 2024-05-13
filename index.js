0e4037917d8f8a82040837fb3be7d128e74b9009
tuioi989 <roxxiemoonpun0suy@hotmail.com>
2024-05-14 01:54:30 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function fibonacci(n) {
-  if (n <= 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  if (obstacleGrid[0][0] === 1) return 0;
+  dp[0][0] = 1;
+  for (let i = 1; i < m; i++) {
+    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
+  }
+  for (let j = 1; j < n; j++) {
+    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
 }
